665e641fd7c90c3d78679bb6820c68092309476e
MUNIF <munif.shellbeehaken@gmail.com>
2023-03-20 17:55:27 +0600

    add google provider in login page.

---
+++ pages/login.js
@@ -0,0 +1,31 @@
+import { getProviders, signIn } from 'next-auth/react';
+export default function LoginPage({ providers }) {
+  return (
+    <>
+      <h1 className='flex items-center justify-center h-screen'>
+        {Object.values(providers).map((provider) => (
+          <div>
+            <button
+              onClick={async () => {
+                await signIn(provider.id);
+              }}
+              className='bg-twitterWhite pl-3 pr-5 py-2 text-black rounded-full flex items-center'
+            >
+              <img src='/google.png' alt='' className='h-8' />
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </h1>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
